e9b7b06dfab16b1426a9ab8903d277b61e35da9b
Yeonji Jang <yunji0220@naver.com>
2021-09-29 17:44:01 +0900

    second commit

--- Test/src/mypack/Bonus.java
+++ Test/src/mypack/Bonus.java
@@ -7,7 +7,7 @@ public class Bonus {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int mysales, bonus;
-		final int target = 1000;
+		final int target = 1000;   // 변하지 않는 상수. 스스로 오류를 탐지할 떄? 사용
 		Scanner input = new Scanner(System.in);
 		System.out.print("내가 판매한 금액을 입력하세요: ");
 		//클래스명.변수.메소드명()
