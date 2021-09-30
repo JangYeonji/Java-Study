e09a238ef0f199d2aff34fc66687d62b1fa313ac
Yeonji Jang <yunji0220@naver.com>
2021-09-30 17:42:23 +0900

    fifth commit

--- Test/src/T210930/gugudan.java
+++ Test/src/T210930/gugudan.java
@@ -4,12 +4,15 @@ public class gugudan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=2;i<=9;i+=2) {
-			for(int j=1;j<=9;j++) {
-				System.out.println(i+"x"+j+"="+i*j);
-			}
-			System.out.println();
-		}
+			int num=2;
+			for(int j=1;num<=9;j++) {
+				if(j>9) {
+					j = 0;
+					num++;
+					continue;
+				}
+				System.out.println(num+"x"+j+"="+num*j);
+			}		
 	}
 
 }
