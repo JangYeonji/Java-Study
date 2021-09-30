c20762b20e4f2fc26b82ac689ab29ea0085458fb
Yeonji Jang <yunji0220@naver.com>
2021-09-30 17:13:03 +0900

    first commit

--- Test/src/T210930/ArrayTest1.java
+++ Test/src/T210930/ArrayTest1.java
@@ -2,7 +2,7 @@ package T210930;
 
 public class ArrayTest1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {   //static은 메모리에 먼저 올라가는 메소드..?
 		// TODO Auto-generated method stub
 		int [] s = new int [10];   //생성시 크기를 지정. 이후 크기 변경 불가
 		//int s [] = 로도 쓸 수 있음
---
+++ Test/src/T210930/gugudan.java
@@ -0,0 +1,15 @@
+package T210930;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=9;i++) {
+			for(int j=1;j<=9;j++) {
+				System.out.printf("%d * %d = %d, ",i,j,i*j);
+			}
+			System.out.printf("\n");
+		}
+	}
+
+}
