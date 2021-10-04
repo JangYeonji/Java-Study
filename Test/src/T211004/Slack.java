1d22b3d9680ca2b76581841ede3d5859ea2ac386
Yeonji Jang <yunji0220@naver.com>
2021-10-04 17:53:18 +0900

    first commit

---
+++ Test/src/T211004/Slack.java
@@ -0,0 +1,13 @@
+package T211004;
+
+public class Slack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SlackController slack = new SlackController();
+		System.out.println(slack.getNotice());
+		System.out.println(slack.getShareFile());
+
+	}
+
+}
---
+++ Test/src/T211004/SlackController.java
@@ -0,0 +1,12 @@
+package T211004;
+
+public class SlackController {
+	private String noticeUrl = "공지사항 : http://www.~";
+
+	public String getNotice() {
+		return noticeUrl;
+	}
+	public String getShareFile() {
+		return "자료공유입니다.";
+	}
+}
